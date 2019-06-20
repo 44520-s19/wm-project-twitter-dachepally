126ae677d54ae9849377ed4fddc1e7a3cff8ff19
Sai Sri Dachepalli <s534883@nwmissouri.edu>
2019-06-19 22:02:50 -0500

    pushing files

---
+++ twitter_keys.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 18 00:58:36 2019
+
+@author: S534883
+"""
+
+ 
+consumer_key = 'ZWQCKrPiKLG7qXBcPMW80LlDO'
+consumer_secret = '1Wtn9o7E696mLib7P4S7jemCHaYUtp6wVLAUetqNaIgYdnTbk0'
+access_token = '1087703281168773121-2LHQhIZuvZ4mvi8LHJ6HUKB7osaIL2'
+access_secret = 'Aog0QMkoyEWi2zYuAEHCWNHoficQT4YHy2qqTjsHbjXpI' 
+ 
